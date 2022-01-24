4aa23570727ce358f44ae0bdfd2617ad10efff4f
e-petrovich2022 <dev081023@gmail.com>
2022-01-24 07:37:06 +0200

    Форма настройки плагина GalleryBehavior

--- src/Plugin/paragraphs/Behavior/GalleryBehavior.php
+++ src/Plugin/paragraphs/Behavior/GalleryBehavior.php
@@ -5,8 +5,10 @@ namespace Drupal\dlog_paragraphs\Plugin\paragraphs\Behavior;
 
 
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
 use Drupal\paragraphs\ParagraphsBehaviorBase;
 
 /**
@@ -28,6 +30,23 @@ class GalleryBehavior extends ParagraphsBehaviorBase {
 
   public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
 
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form['items_per_row'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Number of images per row'),
+      '#options' => [
+        '2' => $this->formatPlural(2, '1 photo per row', '@count photos per row'),
+        '2' => $this->formatPlural(3, '1 photo per row', '@count photos per row'),
+        '2' => $this->formatPlural(4, '1 photo per row', '@count photos per row'),
+      ],
+    ];
+
+    return $form;
   }
 
 }
